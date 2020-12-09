fcd3705c706402c78b5d907c61b0c52b24a7f3b2
Filipe Plucenio <plucenio@gmail.com>
2020-12-09 16:33:45 -0300

    New version.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "779e4407538f0ef1c54b84dbb99cfe80",
-"index.html": "68b7b1f872205a813b1ecbd68f29d5b1",
-"/": "68b7b1f872205a813b1ecbd68f29d5b1",
-"main.dart.js": "8b2f9e714ea78914afbc0a088dfe3f88",
+"index.html": "700a8ff40b6c4602828b0ff75e11132a",
+"/": "700a8ff40b6c4602828b0ff75e11132a",
+"main.dart.js": "6a6d899a77dfe94c3f739d6308212002",
 "web.config": "f5fe1c4278cebc83b027a1f78c6c6bd4",
 "favicon.png": "cb08503dd068a65b09d0db62578bf0f5",
 "icons/Icon-192.png": "c4553ce1cf349e24ec2c7b03336a09a1",
