4358a6165a0db2c28be3bead5887091582cc5a41
builder <infra@safety4me.com>
2021-02-19 09:30:35 -0800

    New version committed by CodeMagic

--- safety4me_service_worker.js
+++ safety4me_service_worker.js
@@ -1,6 +1,16 @@
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
-  var promise = Promise.resolve();    
-  promise = promise.then(function() { clients.openWindow("https://app.safety4me.com/#/"); });
-  event.waitUntil(promise);  
+  var promise = Promise.resolve();
+  promise = promise.then(function () {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Notification',
+      eventAction: 'clicked',
+      eventLabel: 'test_notification'
+
+    });
+    clients.openWindow("https://app.safety4me.com/#/");
+  }
+  );
+  event.waitUntil(promise);
 });
