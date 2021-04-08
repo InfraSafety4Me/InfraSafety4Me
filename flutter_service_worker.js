463199f24df75686e52238401e6bde182b74accb
Matheus Schipper <infra@safety4me.com>
2021-04-08 06:04:16 -0700

    New version committed by CodeMagic

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "779e4407538f0ef1c54b84dbb99cfe80",
-"index.html": "728a6880ab1c7858a85c3d58d5fedf2d",
-"/": "728a6880ab1c7858a85c3d58d5fedf2d",
+"index.html": "0e2b1103bcdbd6037d86b0bbe087cb57",
+"/": "0e2b1103bcdbd6037d86b0bbe087cb57",
 "safety4me_service_worker.js": "79a07849d4bed63174afbb3ab493c482",
 "main.dart.js": "3eaadc2a33fbd0c5d714a312304521de",
 "web.config": "f5fe1c4278cebc83b027a1f78c6c6bd4",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "7ec73b601ef160284c0784e395583c65",
 "manifest.json": "cd388b4185c0071e90c205df95b3b483",
 "assets/AssetManifest.json": "328e374d3618319a8f9b4d468c0efdaf",
-"assets/NOTICES": "a69ca853bb527e04758f9e00b918be15",
+"assets/NOTICES": "22e6935d97d401909b213912459db472",
 "assets/FontManifest.json": "f2702f96f1e2abe16334a4bf89e38c9f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/flutter_icons/fonts/Octicons.ttf": "73b8cff012825060b308d2162f31dbb2",
