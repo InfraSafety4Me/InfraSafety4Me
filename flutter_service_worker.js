e5a3831c35aceba79285ba77845463bd43f35656
Matheus Schipper <infra@safety4me.com>
2021-04-23 13:18:33 -0700

    New version committed by CodeMagic

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "779e4407538f0ef1c54b84dbb99cfe80",
-"index.html": "3a0c548dfcdf21bcdbc66c08b29d58e3",
-"/": "3a0c548dfcdf21bcdbc66c08b29d58e3",
+"index.html": "da3bdf494631d4e0104268a48998495b",
+"/": "da3bdf494631d4e0104268a48998495b",
 "safety4me_service_worker.js": "79a07849d4bed63174afbb3ab493c482",
-"main.dart.js": "a33117e1bd8fcd15ca5f814660b62361",
+"main.dart.js": "5e1c6005af806e3122aca3de210ab61b",
 "web.config": "f5fe1c4278cebc83b027a1f78c6c6bd4",
 "favicon.png": "cb08503dd068a65b09d0db62578bf0f5",
 "icons/Icon-192.png": "c4553ce1cf349e24ec2c7b03336a09a1",
