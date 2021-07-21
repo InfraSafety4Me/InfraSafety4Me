86893b94ba15c2b7115514690cee58e9cd3f324e
administrador <infra@safety4me.com>
2021-07-21 21:50:48 +0000

    New version committed by CodeMagic

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "779e4407538f0ef1c54b84dbb99cfe80",
-"index.html": "9e332755896cfb62ae7ca9d8563c9b7a",
-"/": "9e332755896cfb62ae7ca9d8563c9b7a",
+"index.html": "46d5d75332c3ae8ba765fa399f0ae9e0",
+"/": "46d5d75332c3ae8ba765fa399f0ae9e0",
 "safety4me_service_worker.js": "79a07849d4bed63174afbb3ab493c482",
-"main.dart.js": "9f0c4d9ff5ce31575b22cabc238cfcab",
+"main.dart.js": "75ae5cf9ba727fc9767198544c9f8e7c",
 "web.config": "f5fe1c4278cebc83b027a1f78c6c6bd4",
 "favicon.png": "cb08503dd068a65b09d0db62578bf0f5",
 "icons/Icon-192.png": "c4553ce1cf349e24ec2c7b03336a09a1",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "7ec73b601ef160284c0784e395583c65",
 "manifest.json": "cd388b4185c0071e90c205df95b3b483",
 "assets/AssetManifest.json": "9cccdf467bfeb4b90566049b658f4fe2",
-"assets/NOTICES": "9330af61befd739eb19e7349ad2288c0",
+"assets/NOTICES": "3987c47cb400862a8e34b08198245af2",
 "assets/FontManifest.json": "a0f0928972ac9d8ccb2890e7994b92b7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "ffed6899ceb84c60a1efa51c809a57e4",
