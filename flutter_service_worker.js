0d215bee3c897271afa3b27dd63dfa05cafb1764
felipebb <infra@safety4me.com>
2021-10-11 13:31:04 +0000

    New version committed by CodeMagic

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "779e4407538f0ef1c54b84dbb99cfe80",
-"index.html": "56a992d5b13995a52f391e090e79870f",
-"/": "56a992d5b13995a52f391e090e79870f",
+"index.html": "353de0c1a5cd1ef95710cb749673b55e",
+"/": "353de0c1a5cd1ef95710cb749673b55e",
 "safety4me_service_worker.js": "79a07849d4bed63174afbb3ab493c482",
-"main.dart.js": "c3b07ef101b8a80be94fd62d908de68d",
+"main.dart.js": "6cf62cce7ec6d9fd2e6a4df2bf2fca1c",
 "web.config": "f5fe1c4278cebc83b027a1f78c6c6bd4",
 "favicon.png": "cb08503dd068a65b09d0db62578bf0f5",
 "icons/Icon-192.png": "c4553ce1cf349e24ec2c7b03336a09a1",
